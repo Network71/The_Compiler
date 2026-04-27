8d666f1c59d5b4630facd032b89898769a06c022
Network71 <bwiniarski2007@gmail.com>
2026-04-27 14:24:54 +0100

    debuggingV2

--- debugging_code.py
+++ debugging_code.py
@@ -6,15 +6,13 @@ def check_input(input):
     print("check input")
     correct_answer = 'print("Hello, world!")'
 
-    if input == correct_answer:
-        correct_label = Label(debug_window, text='Correct! Proceed to next level', font=('Calibri', 20))
-        correct_label.pack(side='top')
 
-        #move on here
-        
+    if input == correct_answer:
+        info_label.config(text='Correct, proceed to the next level!')
+        #move on here           
+
     else:
-        incorrect_label = Label(debug_window, text='Incorrect, try again', font=('Calibri', 20))
-        incorrect_label.pack(side='top')
+        info_label.config(text='Incorrect, try again!')
 
 def get_input():
     input_code = code_entry.get()
@@ -25,6 +23,7 @@ def debugging_window():
     global debug_window 
     debug_window = Tk()
     debug_window.title("Debugging Code")
+    debug_window.geometry('500x300')
 
     head_label = Label(debug_window, text='Find the mistake!', font=('Calibri', 30))
     head_label.pack(side='top')
@@ -36,17 +35,22 @@ def debugging_window():
     label.pack()
 
     #secondary label
-    second_label = Label(debug_window, text='Enter the correct code below!')
+    second_label = Label(debug_window, text='Enter the correct code below!', font=('Calibri', 15))
     second_label.pack(side='top')
 
     #input field for correct code
     global code_entry
-    code_entry = Entry(debug_window)
+    code_entry = Entry(debug_window, width=25)
     code_entry.pack(side='top')
 
     #check button
-    check_button = Button(debug_window, text='Check', command=get_input)
+    check_button = Button(debug_window, text='Check', font=('Calibri', 10), command=get_input, padx=20, pady=10)
     check_button.pack(side='top')
+
+    #feedback label
+    global info_label
+    info_label = Label(debug_window, font=('Calibri', 20))
+    info_label.pack(side='top')
 
     debug_window.mainloop()
 
